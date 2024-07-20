cef9941d283d9d0314f9785e18e718fd9d17f6e6
GorkemKurtkaya <gorkem.kurtkaya@yahoo.com>
2024-07-20 20:56:34 +0300

    .

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import conn from './db.js';
+import pageRoute from './routes/pageRoute.js';
 
 
 dotenv.config();
@@ -18,14 +19,17 @@ app.set('view engine', 'ejs');
 //static dosyası
 app.use(express.static('public'));
 
+//routes
+app.use("/",pageRoute)
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
 
-app.get('/about', (req, res) => {
-    res.render('about');
-});
+// app.get('/', (req, res) => {
+//     res.render('index');
+// });
+
+// app.get('/about', (req, res) => {
+//     res.render('about');
+// });
 
 
 app.listen(PORT, () => {
