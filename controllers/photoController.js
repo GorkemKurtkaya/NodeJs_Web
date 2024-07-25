cf9340e14878bfea037fc1be7ac43be5b7c7ca4c
GorkemKurtkaya <gorkem.kurtkaya@yahoo.com>
2024-07-25 18:38:52 +0300

    .

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -1,50 +1,74 @@
 import Photo from "../models/photomodel.js";
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
 
 
-const createPhoto = async (req,res) => {
-    try{
+const createPhoto = async (req, res) => {
+
+    const result = await cloudinary.uploader.upload(req.files.image.tempFilePath, {
+        use_filename: true,
+        folder: "lenslight_tr"
+    });
+
+
+    try {
         await Photo.create({
-            name:req.body.name,
-            description:req.body.description,
-            user:res.locals.user._id
+            name: req.body.name,
+            description: req.body.description,
+            user: res.locals.user._id,
+            url: result.secure_url,
         });
+        fs.unlinkSync(req.files.image.tempFilePath);
+
+
         res.redirect("users/dashboard");
-    }catch(error){
+    } catch (error) {
         res.status(400).json({
-            success:false,
-            error:error.message
+            success: false,
+            error: error.message
         });
     }
 }
 
-const getAllPhotos = async (req,res) => {
-    try{
-        const photos=await Photo.find();
-        res.status(200).render("photos",{
+const getAllPhotos = async (req, res) => {
+    try {
+        const photos = res.locals.user ? await Photo.find({ user: { $ne: res.locals.user._id } })
+            :
+            await Photo.find();
+        res.status(200).render("photos", {
             photos,
-            link:"photos"
+            link: "photos"
         })
-    }catch(error){
+        res.status(200).render("photos", {
+            photos,
+            link: "photos"
+        })
+
+
+
+
+
+    } catch (error) {
         res.status(400).json({
-            success:false,
-            error:error.message
+            success: false,
+            error: error.message
         });
     }
 }
 
-const getAPhoto = async (req,res) => {
-    try{
-        const photo=await Photo.findById({_id : req.params.id});
-        res.status(200).render("photo",{
+const getAPhoto = async (req, res) => {
+    try {
+        const photo = await Photo.findById({ _id: req.params.id }).populate("user");
+        res.status(200).render("photo", {
             photo,
-            link:"photo"
+            link: "photo"
         })
-    }catch(error){
+    } catch (error) {
         res.status(400).json({
-            success:false,
-            error:error.message
+            success: false,
+            error: error.message
         });
     }
 }
 
-export{createPhoto,getAllPhotos,getAPhoto};
+export { createPhoto, getAllPhotos, getAPhoto };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,5 +7,8 @@ const router = express.Router();
 router.route('/register').post(userController.createUser);
 router.route('/login').post(userController.loginUser);
 router.route('/dashboard').get(authMiddleWare.authenticateToken, userController.getDashboardPage);
+router.route('/').get(authMiddleWare.authenticateToken, userController.getAllUsers);
+router.route('/:id').get(authMiddleWare.authenticateToken, userController.getAUsers);
+
 
 export default router;
