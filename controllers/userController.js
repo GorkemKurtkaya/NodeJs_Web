47d62bff2d05abfdeebc74d629a68e5721eb0afc
GorkemKurtkaya <gorkem.kurtkaya@yahoo.com>
2024-07-30 15:11:26 +0300

    .

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 import User from "../models/usermodel.js";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken"; 
+import jwt from "jsonwebtoken";
 import Photo from "../models/photomodel.js";
 
 
@@ -14,42 +14,42 @@ const createUser = async (req, res) => {
             user: user._id
         });
     } catch (error) {
-        
 
-        let errors2 ={}
+
+        let errors2 = {}
 
         if (error.code === 11000) {
             errors2.email = "Email is already registered";
         }
-        
 
-        if(error.name === "ValidationError"){
+
+        if (error.name === "ValidationError") {
             Object.keys(error.errors).forEach((key) => {
                 errors2[key] = error.errors[key].message;
-            
+
             });
         }
-            
+
 
         res.status(400).json({
             succeded: false,
             errors: errors2
-        }); 
-        
+        });
+
     }
 }
 
 const loginUser = async (req, res) => {
     try {
-        const {username, password} = req.body;
+        const { username, password } = req.body;
 
-        const user = await User.findOne({username});
+        const user = await User.findOne({ username });
 
         let same = false;
         if (user) {
             same = await bcrypt.compare(password, user.password);
-            
-        }else{
+
+        } else {
             return res.status(401).json({
                 succeded: false,
                 message: "Invalid credentials"
@@ -63,21 +63,21 @@ const loginUser = async (req, res) => {
             });
 
             res.redirect("/users/dashboard");
-            
+
         } else {
             res.status(400).send("Invalid credentials");
         }
-    } 
+    }
 
-    
+
 
     catch (error) {
         res.status
-        (500).send(error);
+            (500).send(error);
     }
 }
 
-const createToken=(userId) => {
+const createToken = (userId) => {
     return jwt.sign({
         userId
     }, process.env.JWT_SECRET, {
@@ -86,17 +86,18 @@ const createToken=(userId) => {
 }
 
 const getDashboardPage = async (req, res) => {
-    const photos= await Photo.find({user:res.locals.user._id});
-    res.render('dashboard',{
-        link:"dashboard",
+    const photos = await Photo.find({ user: res.locals.user._id });
+    const user = await User.findById({ _id: res.locals.user._id }).populate("followings").populate("followers");
+    res.render('dashboard', {
+        link: "dashboard",
         photos,
-    
+
     });
 }
 
 const getAllUsers = async (req, res) => {
     try {
-        const users = await User.find({_id:{$ne:res.locals.user._id}});
+        const users = await User.find({ _id: { $ne: res.locals.user._id } });
         res.status(200).render("users", {
             users,
             link: "users"
@@ -111,7 +112,7 @@ const getAllUsers = async (req, res) => {
 const getAUsers = async (req, res) => {
     try {
         const user = await User.findById({ _id: req.params.id });
-        const photos = await Photo.find({ user: res.locals.user._id });
+        const photos = await Photo.find({ user: user._id });
         res.status(200).render("user", {
             user,
             photos,
@@ -125,5 +126,49 @@ const getAUsers = async (req, res) => {
         });
     }
 }
+const follow = async (req, res) => {
+    try {
+        let user = await User.findByIdAndUpdate({ _id: req.params.id }, {
+            $push: {
+                followers: res.locals.user._id
+            }
+        }, { new: true });
 
-export { createUser, loginUser, getDashboardPage,getAllUsers,getAUsers };
+        user = await User.findByIdAndUpdate({ _id: res.locals.user._id }, {
+            $push: {
+                followings: req.params.id
+            }
+        }, { new: true });
+
+    }
+    catch (error) {
+        res.status(400).json({
+            succeded: false,
+            error: error.message
+        });
+    }
+}
+const unfollow = async (req, res) => {
+    try {
+        let user = await User.findByIdAndUpdate({ _id: req.params.id }, {
+            $pull: {
+                followers: res.locals.user._id
+            }
+        }, { new: true });
+
+        user = await User.findByIdAndUpdate({ _id: res.locals.user._id }, {
+            $pull: {
+                followings: req.params.id
+            }
+        }, { new: true });
+
+    }
+    catch (error) {
+        res.status(400).json({
+            succeded: false,
+            error: error.message
+        });
+    }
+}
+
+export { createUser, loginUser, getDashboardPage, getAllUsers, getAUsers,follow,unfollow };
--- models/usermodel.js
+++ models/usermodel.js
@@ -8,19 +8,31 @@ const userSchema = new Schema({
     username: {
         type: String,
         required: [true, "Username is required"],
-        validate:[validator.isAlphanumeric, "Username can only contain letters and numbers"]
+        validate: [validator.isAlphanumeric, "Username can only contain letters and numbers"]
     },
     email: {
         type: String,
         required: [true, "Email is required"],
         unique: true,
-        validate:[validator.isEmail, "Invalid email address"]
+        validate: [validator.isEmail, "Invalid email address"]
     },
     password: {
         type: String,
         required: [true, "Password is required"],
         minLength: [3, "Password must be at least 3 characters long"]
-    }
+    },
+    followers: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ],
+    followings: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ],
 }, { timestamps: true });
 
 userSchema.pre("save", function (next) {
