ad2e19e7b158c63e8900010c0bcedfd5c90cf286
GorkemKurtkaya <gorkem.kurtkaya@yahoo.com>
2024-07-30 15:14:09 +0300

    .

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -9,6 +9,7 @@ router.route('/login').post(userController.loginUser);
 router.route('/dashboard').get(authMiddleWare.authenticateToken, userController.getDashboardPage);
 router.route('/').get(authMiddleWare.authenticateToken, userController.getAllUsers);
 router.route('/:id').get(authMiddleWare.authenticateToken, userController.getAUsers);
-
+router.route('/:id/follow').put(authMiddleWare.authenticateToken, userController.follow);
+router.route('/:id/follow').put(authMiddleWare.authenticateToken, userController.unfollow);
 
 export default router;
